d2a0f0d18ff7a5b33e3b363b4927b19a13f3dab0
ZTao-z <1124693098@qq.com>
2021-04-17 18:27:14 +0800

    feat: 搭建ts开发测试环境

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { sum } from './subTest/p';
+
+class NxtReaderCore {
+  version: string = "1.0.0"
+
+  constructor() {
+    sum(1, 2)
+  }
+}
+
+export { NxtReaderCore, sum } ; 
+
---
+++ src/subTest/p.ts
@@ -0,0 +1,5 @@
+function sumFunc(a: number, b: number) {
+  return a + b;
+}
+
+export const sum = sumFunc;
