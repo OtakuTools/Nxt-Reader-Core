acc50a90f7837152a8655943924ce026e24f5e87
Ming <184653774@qq.com>
2021-04-18 15:41:37 +0800

    feat: 添加Nxt-Reader-Core实例化测试样例

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,14 @@
-import { sum } from '../src/main'
+import { sum, NxtReaderCore } from '../src/main'
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+test('Nxt-Reader-Core Can be Created', () => {
+  const NxtReaderCoreConstrutor = jest.fn(_ => new NxtReaderCore())
+  const NxtReaderCoreInstance = NxtReaderCoreConstrutor()
+  expect(NxtReaderCoreConstrutor).toReturn()
+  expect(NxtReaderCoreInstance).toBeInstanceOf(NxtReaderCore)
+  expect(NxtReaderCoreInstance).toHaveProperty('version')
+  expect(NxtReaderCoreInstance.version).toMatch(/^\d\.\d\.\d$/)
+})
